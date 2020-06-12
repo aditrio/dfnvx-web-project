10b88ab2f984087b2e4309ae5af8ec4fd5d4a46a
adi__try <48681463+aditrio@users.noreply.github.com>
2020-06-13 05:05:11 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="./assets/css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
     
     <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
